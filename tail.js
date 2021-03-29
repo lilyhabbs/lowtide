26f1eb3fcb464df1ca3d67bd7e3ce3a85993312e
Lily Habberfield <lilyhabbs@gmail.com>
2021-03-29 22:17:03 +0000

    Created a function to retrieve all elements, except the first, from an array

---
+++ tail.js
@@ -0,0 +1,30 @@
+// tests code to compare if actual result matches expected result
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// returns all elements in an array, except for the head (first item)
+const tail = array => array.slice(1);
+
+// TEST CASE: Check the returned array elements
+const result = tail(['Hello', 'Lighthouse', 'Labs']);
+assertEqual(result.length, 2);
+assertEqual(result[0], 'Lighthouse');
+assertEqual(result[1], 'Labs');
+
+// TEST CASE: Check the returned array elements for empty array
+const resultEmpty = tail([]);
+assertEqual(resultEmpty.length, 0);
+
+// TEST CASE: Check the returned array elements for an array with one element
+const resultOne = tail([]);
+assertEqual(resultOne.length, 0);
+
+// TEST CASE: Check if original array has not been modified
+const words = ['Yo Yo', 'Lighthouse', 'Labs'];
+tail(words);
+assertEqual(words.length, 3);
