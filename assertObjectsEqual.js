cf687823e0b22b4efe6f536849b42d45e2d2c4f4
Lily Habberfield <lilyhabbs@gmail.com>
2021-03-31 22:47:17 +0000

    Added new function to compare objects

---
+++ assertObjectsEqual.js
@@ -0,0 +1,39 @@
+const eqArrays = (array1, array2) => {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+// Returns true if both objects have identical keys with identical values
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } else {
+    for (const key1 in object1) {
+      for (const key2 in object2) {
+        if (Array.isArray(object1[key1]) && Array.isArray(object2[key2])) {
+          return eqArrays(object1[key1], object2[key2]);
+        } else if (key2 === key1 && object1[key1] !== object2[key2]) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`🟢 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
