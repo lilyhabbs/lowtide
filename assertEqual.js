061fe2d79da6fc1485524bb1a81843a50c46e80b
Lily Habberfield <lilyhabbs@gmail.com>
2021-04-05 14:43:35 +0000

    Removed test data from code and created separate test files

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,9 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-assertEqual('Hello', 'Hello');
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1, 1);
-assertEqual(3, 12);
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual('Hello', 'Hello');
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual(3, 12);
