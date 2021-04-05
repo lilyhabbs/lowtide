73f4572f04ea6b9e81fa90dae01752c8b3caefac
Lily Habberfield <lilyhabbs@gmail.com>
2021-04-05 15:12:41 +0000

    Removed test data from eqArrays.js and created separate test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
+// compares two arrays
 const eqArrays = (array1, array2) => {
   if (array1.length !== array2.length) {
     return false;
@@ -18,9 +13,5 @@ const eqArrays = (array1, array2) => {
     return true;
   }
 };
- 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
